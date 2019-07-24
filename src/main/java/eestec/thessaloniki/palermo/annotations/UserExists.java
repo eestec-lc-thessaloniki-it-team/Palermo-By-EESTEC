69d68a8fbd5d0fe56262b7b10c533b77155593e3
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-07-24 12:35:31 +0200

    created the first interceptor to test if a username exists

---
+++ src/main/java/eestec/thessaloniki/palermo/annotations/UserExists.java
@@ -0,0 +1,19 @@
+package eestec.thessaloniki.palermo.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.interceptor.InterceptorBinding;
+
+
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD,ElementType.TYPE})
+@Inherited
+public @interface UserExists {
+   
+    
+    
+}
