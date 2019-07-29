cdb06cd2e76a6f457df97d13431da1689d5800e5
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-07-29 17:42:25 +0200

    there was an error with the else statement that returned 401 withour reason

--- src/main/java/eestec/thessaloniki/palermo/security/AuthorizedUserInceptor.java
+++ src/main/java/eestec/thessaloniki/palermo/security/AuthorizedUserInceptor.java
@@ -27,33 +27,33 @@ public class AuthorizedUserInceptor {
     public Object authorizedUser(InvocationContext invocationContext) throws Exception {
         if (invocationContext.getMethod().getName().equals("logInUser")) {
             User user = (User) invocationContext.getParameters()[0];
-            if(user.getUsername().equals("") || user.getPassword().equals("")){
+            if (user.getUsername().equals("") || user.getPassword().equals("")) {
                 return Response.status(400).build();
             }
-            User u = userService.findUser(user); 
-            if (u==null){
+            User u = userService.findUser(user);
+            if (u == null) {
                 return Response.status(410, "Wrong username or password").build();
             }
             return invocationContext.proceed();
-            
+
         } else {
             //for the rest that we want to be connected and have the same token
             System.out.println("Want to check if my credentials are correct");
             for (Object param : invocationContext.getParameters()) {
+                System.out.println(param.getClass());
                 if (param.getClass().equals(UserToken.class)) { //finding the UserToken in params
-                    UserToken userToken=(UserToken)param;
-                    if(userTokenService.isValid(userToken)){
+                    UserToken userToken = (UserToken) param;
+                    System.out.println(userToken.toString());
+                    if (userTokenService.isValid(userToken)) {
                         return invocationContext.proceed();
-                    }else{
+                    } else {
                         return Response.status(401, "Unauthorized").build();
                     }
-                }else{
-                    return Response.status(401, "Unauthorized").build();
                 }
             }
-
+            return Response.status(400).build();
         }
-        return Response.status(400).build();
+
     }
 
 }
