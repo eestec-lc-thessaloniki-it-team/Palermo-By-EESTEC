6fff0dc164bee12e1564d78b236ece448c6b06c4
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-07-30 13:53:20 +0200

    TestCase3 is about starting the game, only from the leader, needed to add the tests for the role type after the logic is filled

---
+++ tests_python/TestCase3.py
@@ -0,0 +1,65 @@
+import sys
+
+import requests
+
+from printing import printin
+
+
+def testCase3(serverIP, username, password, username2, password2):
+    '''
+    This test case will demonstrate the begining of a game. 2 users will connect. Both will try to start the game,
+    but only the leader should succeed. N end point should exist that will return the info for the game. When the game
+    starts appropriate the roles should be given to players and update their user_to_game record
+    :param serverIP: servers ip that app is uploaded
+    :param username: username of user
+    :param password: password of user
+    :param username2:  username of user2
+    :param password2: password of user2
+    :return:
+    '''
+
+    print("\nTestCase#3 The game starts...\n")
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/user/newUser",
+                      json={"username": username, "password": password})
+    if r.status_code != 200: sys.exit("Error with initialization user1")
+    userID1 = r.json()
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/user/newUser",
+                      json={"username": username2, "password": password2})
+    if r.status_code != 200: sys.exit("Error with initialization user2")
+    userID2 = r.json()
+
+    print("Initialization ...OK")
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/newGame", json=userID1)
+    if r.status_code != 200: sys.exit("There was an error with creating a new game")
+    gameID = r.json()['random_id']
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/joinGame/" + gameID + "D", json=userID2)
+    printin(r.status_code == 404, "Joining an no-existing game")
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/joinGame/" + gameID, json=userID2)
+    if r.status_code != 200: sys.exit("There was an error with joining the correct game")
+
+    r=requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/gameInfo/" + gameID, json=userID1)
+    printin(r.status_code == 200 and not r.json()['started'], "Game Info request")
+    print(r.json())
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/startGame/" + gameID, json=userID2)
+    printin(r.status_code == 401, "A no leader try to start the game")
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/startGame/" + gameID, json=userID1)
+    printin(r.status_code == 200 and r.json()['started'], "Leader starts the game")
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/game/gameInfo/" + gameID, json=userID1)
+    printin(r.status_code == 200 and r.json()['started'], "Game Info request")
+
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/user/logOut", json=userID1)
+    if r.status_code != 200: sys.exit("Error with clearing data")
+
+    r = requests.post("http://" + serverIP + ":8080/palermo/api/v1/user/logOut", json=userID2)
+    if r.status_code != 200: sys.exit("Error with clearing data")
+
+    print("Clearing data ... OK")
--- tests_python/tests.py
+++ tests_python/tests.py
@@ -2,12 +2,13 @@ from TestCase1 import testCase1
 from TestCase2 import testcase2
 from TestCreateUser import testCreateUser, deletingUser
 from TestLogIn import testLogIn
+from TestCase3 import testCase3
 
 serverIP = "127.0.0.1"
 username1="test2"
 password="1234"
 username2="test3"
-
+#
 testCreateUser(serverIP, username1, password)
 userToken = testLogIn(serverIP, username1, password)
 deletingUser(serverIP, username1, password)
@@ -16,5 +17,9 @@ testCase1(serverIP, username1, password)
 deletingUser(serverIP, username1, password)
 
 testcase2(serverIP, username1, password, username2, password)
+deletingUser(serverIP, username1, password)
+deletingUser(serverIP, username2, password)
+
+testCase3(serverIP,username1,password,username2,password)
 deletingUser(serverIP, username1, password)
 deletingUser(serverIP, username2, password)
