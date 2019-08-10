6bf4ebfb536cf7f2435db50ce42f06443ffb387b
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-08-10 10:30:42 +0200

    minor change

--- tests_python/tests.py
+++ tests_python/tests.py
@@ -11,20 +11,20 @@ password = "1234"
 username2 = "test3"
 username3 = "test4"
 #
-# testCreateUser(serverIP, username1, password)
-# userToken = testLogIn(serverIP, username1, password)
-# deletingUser(serverIP, username1, password)
-#
-# testCase1(serverIP, username1, password)
-# deletingUser(serverIP, username1, password)
-#
-# testcase2(serverIP, username1, password, username2, password)
-# deletingUser(serverIP, username1, password)
-# deletingUser(serverIP, username2, password)
-#
-# testCase3(serverIP, username1, password, username2, password)
-# deletingUser(serverIP, username1, password)
-# deletingUser(serverIP, username2, password)
+testCreateUser(serverIP, username1, password)
+userToken = testLogIn(serverIP, username1, password)
+deletingUser(serverIP, username1, password)
+
+testCase1(serverIP, username1, password)
+deletingUser(serverIP, username1, password)
+
+testcase2(serverIP, username1, password, username2, password)
+deletingUser(serverIP, username1, password)
+deletingUser(serverIP, username2, password)
+
+testCase3(serverIP, username1, password, username2, password)
+deletingUser(serverIP, username1, password)
+deletingUser(serverIP, username2, password)
 
 testCase4(serverIP, username1, password, username2, password, username3, password)
 deletingUser(serverIP, username1, password)
