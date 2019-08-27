fec10f31fb54d361a0437f290ae02c5fafb947a0
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-08-27 10:52:47 +0200

    finished checking vote, voteservice, votingstate, everything seems to work as expected

--- src/main/java/eestec/thessaloniki/palermo/game/states/VotingState.java
+++ src/main/java/eestec/thessaloniki/palermo/game/states/VotingState.java
@@ -28,35 +28,39 @@ public class VotingState {
     private final SecureRandom random = new SecureRandom();
 
     /**
-     * This is what happens the first time that we start the voting. All users will be added with 0 votes in the vote table
-     * @param game_id 
+     * This is what happens the first time that we start the voting. All users
+     * will be added with 0 votes in the vote table
+     *
+     * @param game_id
      */
     public void initliazeVoteState(int game_id) {
         voteService.deleteVotes(game_id);
-        List<UserToGame> userToGames=userToGameService.userToGameList(game_id);
+        List<UserToGame> userToGames = userToGameService.userToGameList(game_id);
         this.initlializeUserToGame(userToGames);
-        this.insertCandidates(userToGames,game_id);
+        this.insertCandidates(userToGames, game_id);
         this.chooseNextToVote(game_id);
     }
 
-    private void insertCandidates(List<UserToGame> utgs, int game_id){
-        for (UserToGame utg:utgs){
+    private void insertCandidates(List<UserToGame> utgs, int game_id) {
+        for (UserToGame utg : utgs) {
             voteService.insert(new Vote(game_id, utg.getUser_id(), 0));
         }
     }
-    
-    private void initlializeUserToGame(List<UserToGame> utgs){
-        for (UserToGame utg : utgs ) {
+
+    private void initlializeUserToGame(List<UserToGame> utgs) {
+        for (UserToGame utg : utgs) {
             utg.setIs_voting(false);
             utg.setHas_vote(false);
             userToGameService.update(utg);
         }
     }
-    
+
     public Response vote(WrapperUserTokenVote userTokenVote) {
         UserToGame utg = userToGameService.findByUserId(userTokenVote.getUserToken().getUser_id());
         voteService.voted(userTokenVote.getDeadUser_id());
-        this.chooseNextToVote(utg.getGame_id());
+        if (!this.chooseNextToVote(utg.getGame_id())) {
+            this.whenVoteEnds(utg.getGame_id());
+        }
         return Response.ok().build();
 
     }
@@ -69,32 +73,57 @@ public class VotingState {
         }
         JsonObjectBuilder.add("votes", jsonVotes.build());
         JsonObjectBuilder.add("whoIsVoting", userToGameService.getWhoIsVoting(game_id).getUser_id());
-        JsonObjectBuilder.add("is_voting_over", userToGameService.isVotingOver(game_id));
+        JsonObjectBuilder.add("is_voting_over", userToGameService.isVotingOver(game_id) && voteService.isVotingOver(game_id));
         return Response.ok(JsonObjectBuilder.build()).build();
     }
-    
-    private void whenVoteEnds(int game_id){
+
+    private void whenVoteEnds(int game_id) {
         List<Vote> votes = voteService.getCurrentVotes(game_id);
-        int countSameVotes=1;
-        for(int i=1;i<votes.size();i++){
-            if(votes.get(0).getVotes()==votes.get(i).getVotes()){
+        int countSameVotes = 1;
+        for (int i = 1; i < votes.size(); i++) {
+            if (votes.get(0).getVotes() == votes.get(i).getVotes()) {
                 countSameVotes++;
             }
         }
-        if(countSameVotes==votes.size()){//then we will choose with random who is going to die
-            //kill them all
-        }else{// we will keep the first countSameVotes and add them to the votes again, if it is 1 then we kill him
-            if(countSameVotes==1){ // one has the most 
-                //kill this one
-            }else{// initialize the voting again
-                
+        if (countSameVotes == votes.size()) {//then we will kill them all
+            this.killThemAll(votes);
+            voteService.deleteVotes(game_id);
+        } else if (countSameVotes == 1) { // one has the most 
+            this.killOnePlayer(votes.get(0));
+            voteService.deleteVotes(game_id);
+        } else {// initialize the voting again
+            voteService.deleteVotes(game_id);
+            List<UserToGame> usersToGame = userToGameService.userToGameList(game_id);
+            this.initlializeUserToGame(usersToGame);
+            this.insertCandidates(this.getFirstNUsersInGame(votes, usersToGame, countSameVotes), game_id);
+            this.chooseNextToVote(game_id);
+        }
+    }
+
+    private List<UserToGame> getFirstNUsersInGame(List<Vote> votes, List<UserToGame> usersToGame, int firstN) {
+        List<UserToGame> utgs = new ArrayList<>();
+        for (int i = 0; i < firstN; i++) {
+            for (UserToGame utg : usersToGame) {
+                if (utg.getUser_id() == votes.get(i).getUser_id()) {
+                    utgs.add(utg);
+                    break;
+                }
             }
         }
-        // delete the votes
+        return utgs;
     }
-    
-    private void killThemAll(List<Vote> votes){
-        return;
+
+    private void killThemAll(List<Vote> votes) {
+        for (Vote v : votes) {
+            this.killOnePlayer(v);
+        }
+    }
+
+    private void killOnePlayer(Vote v) {
+        UserToGame userToGame = userToGameService.findByUserId(v.getUser_id());
+        userToGame.setIs_dead(true);
+        userToGame.setIsDeadVisible(true);
+        userToGameService.update(userToGame);
     }
 
     /**
@@ -112,7 +141,7 @@ public class VotingState {
             if (utgs.get(0).isHas_vote()) {// we are at the last
                 return false;
             } else {// we are at the first time
-                utg = utgs.get(random.nextInt(utgs.size() ));
+                utg = utgs.get(random.nextInt(utgs.size()));
                 utg.setIs_voting(true);
                 userToGameService.update(utg);
                 return true;
@@ -123,7 +152,7 @@ public class VotingState {
             userToGameService.update(utg);
             utgs = this.getUsersHaventVote(userToGameService.userToGameList(game_id));
             if (utgs.size() > 0) {
-                utg = utgs.get(random.nextInt(utgs.size() ));
+                utg = utgs.get(random.nextInt(utgs.size()));
                 utg.setIs_voting(true);
                 userToGameService.update(utg);
                 return true;
--- src/test/java/eestec/thessaloniki/palermo/game/states/VotingStateTest.java
+++ src/test/java/eestec/thessaloniki/palermo/game/states/VotingStateTest.java
@@ -6,6 +6,7 @@ import eestec.thessaloniki.palermo.rest.game.GameService;
 import eestec.thessaloniki.palermo.rest.user_to_game.UserToGame;
 import eestec.thessaloniki.palermo.rest.user_to_game.UserToGameService;
 import eestec.thessaloniki.palermo.rest.user_token.UserToken;
+import eestec.thessaloniki.palermo.rest.vote.Vote;
 import eestec.thessaloniki.palermo.rest.vote.VoteService;
 import eestec.thessaloniki.palermo.wrappers.WrapperUserTokenVote;
 import java.security.SecureRandom;
@@ -43,7 +44,8 @@ public class VotingStateTest {
 
     private List<UserToken> userTokens;
     private List<UserToGame> userToGames;
-    private SecureRandom rand=new SecureRandom();
+    private SecureRandom rand = new SecureRandom();
+    int counterForVote;
 
     @Deployment
     public static WebArchive createDeployment() {
@@ -59,6 +61,7 @@ public class VotingStateTest {
     public void initializeTest() {
         userTokens = testMethods.connectUsers();
         userToGames = testMethods.createStartGame(userTokens);
+        counterForVote = 0;
 
     }
 
@@ -67,46 +70,109 @@ public class VotingStateTest {
         testMethods.logoutFromGame(userTokens);
         voteService.deleteVotes(userToGames.get(0).getGame_id());
         gameConnector.endGame(userTokens.get(0));
+        counterForVote = 0;
     }
 
     @Test
-    public void testGetVotes() {
+    public void testOneGetsAtLeast3VotesAndDies() {
         changeStates.changeState(userTokens.get(0)); //change state to morning
         changeStates.changeState(userTokens.get(0)); //change state to voting
         assertEquals("Voting", gameService.searchGameByGameID(userToGames.get(0).getGame_id()).getState()); // check if the state has changed
         UserToGame firstVoter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
         assertNotNull(firstVoter);
         System.out.println("The first voter is " + firstVoter.toString());
-        int voteFor = voteSomeone();
+        int voteFor = everyOneKillOne(2);
         votingState.vote(new WrapperUserTokenVote(findUserToken(firstVoter), voteFor));
         System.out.println("User_id: " + firstVoter.getUser_id() + " has voted for " + voteFor);
         System.out.println(voteService.getCurrentVotes(firstVoter.getGame_id()).toString());
-        //there will be 3 new votes and then we will test the functionality
+
+        makeAVote("all");
+        makeAVote("all");
+        makeAVote("all");
+
+        assertEquals(0, voteService.getCurrentVotes(firstVoter.getGame_id()).size());
+        assertTrue(userToGameService.isVotingOver(firstVoter.getGame_id())); // the voting should be over here
+        assertTrue(voteService.isVotingOver(firstVoter.getGame_id()));
+        List<UserToGame> deadUsers = userToGameService.getDeadPlayers(firstVoter);
+        System.out.println("dead users: " + deadUsers.toString());
+        assertEquals(1, deadUsers.size());
+        assertEquals(true, deadUsers.get(0).isIsDeadVisible());
+    }
+
+    @Test
+    public void test2Get2VotesAndGoToNextState() {
+        changeStates.changeState(userTokens.get(0)); //change state to morning
+        changeStates.changeState(userTokens.get(0)); //change state to voting
+        assertEquals("Voting", gameService.searchGameByGameID(userToGames.get(0).getGame_id()).getState());
         UserToGame voter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
         assertNotNull(voter);
-        assertNotEquals(voter, firstVoter);
-        voteFor = voteSomeone();
+        int voteFor = this.halfHalfVotes();
         votingState.vote(new WrapperUserTokenVote(findUserToken(voter), voteFor));
         System.out.println("User_id: " + voter.getUser_id() + " has voted for " + voteFor);
-        
-        voter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
-        assertNotNull(voter);
-        assertNotEquals(voter, firstVoter);
-        voteFor = voteSomeone();
-        votingState.vote(new WrapperUserTokenVote(findUserToken(voter), voteFor));
-        System.out.println("User_id: " + voter.getUser_id() + " has voted for " + voteFor);
-        
-        voter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
-        assertNotNull(voter);
-        assertNotEquals(voter, firstVoter);
-        voteFor = voteSomeone();
-        votingState.vote(new WrapperUserTokenVote(findUserToken(voter), voteFor));
-        System.out.println("User_id: " + voter.getUser_id() + " has voted for " + voteFor);
-        
-        System.out.println(voteService.getCurrentVotes(firstVoter.getGame_id()).toString());
-        assertTrue(userToGameService.isVotingOver(voter.getGame_id())); // the voting should be over here
-        
 
+        makeAVote("half");
+        makeAVote("half");
+        makeAVote("half");
+
+        List<Vote> votes = voteService.getCurrentVotes(voter.getGame_id());
+        System.out.println("Votes: " + votes.toString());
+        assertEquals(2, votes.size());
+        boolean isVoteOverUserToGame = userToGameService.isVotingOver(voter.getGame_id());
+        boolean isVoteOverVotes = voteService.isVotingOver(voter.getGame_id());
+        assertFalse(isVoteOverVotes);
+        assertFalse(isVoteOverUserToGame);
+        List<UserToGame> deadUsers = userToGameService.getDeadPlayers(voter);
+        System.out.println("dead users: " + deadUsers.toString());
+        assertEquals(0, deadUsers.size());
+
+    }
+
+    @Test
+    public void after2CandidatesChoose1AllOfThem() {
+        this.test2Get2VotesAndGoToNextState();// after this candidates should be 2
+        UserToGame voter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
+        assertNotNull(voter); //someone shpuld vote
+        List<Vote> votes = voteService.getCurrentVotes(voter.getGame_id());
+        assertEquals(2, votes.size()); //between 2
+        int voteFor = this.everyOneKillOne(2);
+        votingState.vote(new WrapperUserTokenVote(findUserToken(voter), voteFor));
+        System.out.println("User_id: " + voter.getUser_id() + " has voted for " + voteFor);
+        makeAVote("all");
+        makeAVote("all");
+        makeAVote("all");
+
+        assertEquals(0, voteService.getCurrentVotes(voter.getGame_id()).size());
+        assertTrue(userToGameService.isVotingOver(voter.getGame_id())); // the voting should be over here
+        assertTrue(voteService.isVotingOver(voter.getGame_id()));
+        List<UserToGame> deadUsers = userToGameService.getDeadPlayers(voter);
+        System.out.println("dead users: " + deadUsers.toString());
+        assertEquals(1, deadUsers.size());
+        assertEquals(8, deadUsers.get(0).getUser_id());
+        assertEquals(true, deadUsers.get(0).isIsDeadVisible());
+
+    }
+
+    @Test
+    public void after2CandidatesKillThemAll() {
+        this.test2Get2VotesAndGoToNextState();// after this candidates should be 2
+        UserToGame voter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
+        assertNotNull(voter); //someone shpuld vote
+        List<Vote> votes = voteService.getCurrentVotes(voter.getGame_id());
+        assertEquals(2, votes.size()); //between 2
+        int voteFor = this.halfHalfVotes();
+        votingState.vote(new WrapperUserTokenVote(findUserToken(voter), voteFor));
+        System.out.println("User_id: " + voter.getUser_id() + " has voted for " + voteFor);
+        makeAVote("half");
+        makeAVote("half");
+        makeAVote("half");
+        assertEquals(0, voteService.getCurrentVotes(voter.getGame_id()).size());
+        assertTrue(userToGameService.isVotingOver(voter.getGame_id())); // the voting should be over here
+        assertTrue(voteService.isVotingOver(voter.getGame_id()));
+        List<UserToGame> deadUsers = userToGameService.getDeadPlayers(voter);
+        System.out.println("dead users: " + deadUsers.toString());
+        assertEquals(2, deadUsers.size());
+        assertEquals(true, deadUsers.get(0).isIsDeadVisible());
+        assertEquals(true, deadUsers.get(1).isIsDeadVisible());
     }
 
     private UserToken findUserToken(UserToGame utg) {
@@ -118,8 +184,31 @@ public class VotingStateTest {
         return null;
     }
 
-    private int voteSomeone() {
-        return userTokens.get(rand.nextInt(4)).getUser_id();
+    private int halfHalfVotes() {
+        counterForVote++;
+        if (counterForVote % 2 == 0) {
+            return userTokens.get(3).getUser_id();
+        } else {
+            return userTokens.get(2).getUser_id();
+        }
+
+    }
+
+    private void makeAVote(String type) {
+        UserToGame voter = userToGameService.getWhoIsVoting(userToGames.get(0).getGame_id());
+        assertNotNull(voter);
+        int voteFor = 6;
+        if (type.equals("all")) {
+            voteFor = everyOneKillOne(2);
+        } else if (type.equals("half")) {
+            voteFor = halfHalfVotes();
+        }
+        votingState.vote(new WrapperUserTokenVote(findUserToken(voter), voteFor));
+        System.out.println("User_id: " + voter.getUser_id() + " has voted for " + voteFor);
+    }
+
+    private int everyOneKillOne(int n) {
+        return userTokens.get(n).getUser_id();
     }
 
 }
